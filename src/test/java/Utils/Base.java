23be909d3024dff42a4605859fc9eda05d5e9ac8
bmahinga <shiluvam@gmail.com>
2025-09-09 19:32:02 +0200

    created runner class

--- src/test/java/Utils/Base.java
+++ src/test/java/Utils/Base.java
@@ -1,5 +1,24 @@
 package Utils;
+import PageObjects.SignupPage;
+import Utils.BrowserFactory; // Adjust the package if needed
+import org.openqa.selenium.WebDriver;
+
+import static Utils.BrowserFactory.driver;
 
 public class Base {
 
+
+
+    public static void setUp(String browser, String url) {
+        driver = BrowserFactory.startBrowser(browser, url);
+        final WebDriver driver = BrowserFactory.startBrowser(browser, "https://gray-island-0bd788c1e.2.azurestaticapps.net/#practice");
+        //signupPage = new SignupPage(driver);
+    }
+
+    public static void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
+
