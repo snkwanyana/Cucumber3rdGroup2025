3fa9b983536c1ce0ced6e8843a378ba6d1bd1cb2
bmahinga <shiluvam@gmail.com>
2025-10-09 16:23:09 +0200

    implemented a report

--- src/test/java/ExtentReports/ExtentReportManager.java
+++ src/test/java/ExtentReports/ExtentReportManager.java
@@ -1,30 +1,41 @@
 package ExtentReports;
 
 import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+import org.openqa.selenium.devtools.v137.page.Page;
 
 import java.io.File;
+import java.lang.reflect.Field;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ExtentReportManager {
 
 
-        private static String reportDir=System.getProperty("user.dir")+"/Reports/ndosiautomation.co.za";
+    private static String reportDir = System.getProperty("user.dir") + "/Reports/ndosiautomation.co.za";
 
-        private static ExtentReports extentReports;
-        private static ExtentSparkReporter extentSparkReporter;
+    private static ExtentReports extentReports;
+    private static ExtentSparkReporter extentSparkReporter;
 
-        public static ExtentReports extentSetup(){
+    public static ExtentReports extentSetup() {
 
-            extentReports = new ExtentReports();
-            extentSparkReporter = new ExtentSparkReporter(new File(reportDir));
-            extentReports.attachReporter(extentSparkReporter);
+        extentReports = new ExtentReports();
+        extentSparkReporter = new ExtentSparkReporter(new File(reportDir));
+        extentReports.attachReporter(extentSparkReporter);
 
-            extentSparkReporter.config().setDocumentTitle("Extent Report");
-            extentSparkReporter.config().setReportName("Ndosi Automation Report");
+        extentSparkReporter.config().setDocumentTitle("Extent Report");
+        extentSparkReporter.config().setReportName("Ndosi Automation Report");
+        extentSparkReporter.config().setReportName("UI Test Results");
+        extentSparkReporter.config().setTheme(Theme.STANDARD);
 
-            extentReports.setSystemInfo("OS",System.getProperty("os.name"));
-            extentReports.setSystemInfo("Exection Machine",System.getProperty("user.name"));
-            return extentReports;
+        extentReports.setSystemInfo("OS", System.getProperty("os.name"));
+        extentReports.setSystemInfo("Exection Machine", System.getProperty("user.name"));
+        return extentReports;
+
+
         }
     }
 
